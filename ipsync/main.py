b8fd0af9a4f92fab797d165b7eb1cc920e51e47d
jon-walton <jonwalton@gmail.com>
2014-12-28 22:29:47 +0800

    fix python 2.7 resolve_ip errors

--- ipsync/main.py
+++ ipsync/main.py
@@ -3,6 +3,7 @@
 import logging
 import requests
 import ipaddress
+import six
 
 from ipsync import providers
 
@@ -17,9 +18,9 @@ def resolve_ip():
     response = requests.get('https://icanhazip.com/')
     if response.status_code == requests.codes['ok']:
         try:
-            ip = ipaddress.ip_address(response.text.strip())
+            ip = ipaddress.ip_address(six.u(response.text.strip()))
         except ValueError as error:
-            logger.error('Could not receive a valid IP address\n%s', error.args)
+            logger.error('Could not receive a valid IP address: %s', error.args)
             return None
 
         logger.info('Received IP address %s', str(ip))

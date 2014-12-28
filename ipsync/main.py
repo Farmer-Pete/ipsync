2bb91d5feeefd4c45243771a39002935c1a4fc32
jon-walton <jonwalton@gmail.com>
2014-12-28 23:48:20 +0800

    moving away from provider factories

--- ipsync/main.py
+++ ipsync/main.py
@@ -5,8 +5,6 @@ import requests
 import ipaddress
 import six
 
-from ipsync import providers
-
 
 def resolve_ip():
     """Resolve the external IP address of this machine.
@@ -28,6 +26,22 @@ def resolve_ip():
     else:
         return None
 
+
+def provider_update_rax():
+    """Use Pyrax to update DNS records configured within Rackspace Cloud DNS.
+
+    If the domains do not already exist, this method will attempt to create them.
+    If the records within the domain do not exist, this method will attempt to create them.
+
+    :return: None
+    """
+    pass
+
 if __name__ == '__main__':
-    test = providers.DnsProvider('rax', 'bla')
-    test.update_records()
+    providers = {'rax', provider_update_rax}
+
+    # read YAML
+    # for each root key in YAML
+    #   update_function = providers.get(key)
+    #   if update_function:
+    #     update_function(config_section)

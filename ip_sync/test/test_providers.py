f2b913bd898f2169e4518c368d401df783d3628f
jon-walton <jonwalton@gmail.com>
2015-01-01 21:38:38 +0800

    added provider tests

--- ip_sync/test/test_providers.py
+++ ip_sync/test/test_providers.py
@@ -1,4 +1,7 @@
 # pylint: disable=C0111,R0903
+from ipaddress import IPv4Address
+import six
+
 from ip_sync.test import TestBase
 from ip_sync import providers
 
@@ -7,8 +10,40 @@ class TestProviders(TestBase):
     def setUp(self):
         super(TestProviders, self).setUp()
 
+    def test_get_invalid_provider(self):
+        self.assertIsInstance(providers.get_provider('invalid-provider-123456', None),
+                              providers.InvalidProvider)
+
     def test_get_providers(self):
-        for provider in self._config_data:
-            class_name = providers.get_provider(provider, self._config_data).__class__.__name__
-            self.assertEqual(provider.lower(), class_name.lower(),
-                             'Provider \'%s\' does not match \'%s\'' % (provider, class_name))
+        provider_data = {
+            'invalid-provider-123456': (None, providers.InvalidProvider),
+            'rackspace': (self._config_data.get('rackspace'), providers.Rackspace),
+            'namecheap': (self._config_data.get('namecheap'), providers.Namecheap),
+        }
+
+        for name in self._config_data:
+            self.assertIsNotNone(provider_data.get(name), 'No Provider exists for \'%s\'' % name)
+            config, class_type = provider_data.get(name)
+            result = providers.get_provider(name, config)
+
+            self.assertIsInstance(result, class_type,
+                                  'result \'%s\' is not an instance of \'%s\''
+                                  % (result.__class__.__name__, class_type))
+
+            self.assertEqual(result._name, name)  # pylint: disable=W0212
+            self.assertEqual(result._config, config)  # pylint: disable=W0212
+
+    def test_invalid_provider_update(self):
+        provider = providers.get_provider('invalid-provider-1234', None)
+        self.assertIsInstance(provider, providers.InvalidProvider)
+        provider.update_ip(IPv4Address(six.u('127.0.0.1')))
+
+    def test_rackspace_update(self):
+        provider = providers.get_provider('rackspace', self._config_data['rackspace'])
+        self.assertIsInstance(provider, providers.Rackspace)
+        provider.update_ip(IPv4Address(six.u('127.0.0.1')))
+
+    def test_namecheap_update(self):
+        provider = providers.get_provider('namecheap', self._config_data['namecheap'])
+        self.assertIsInstance(provider, providers.Namecheap)
+        provider.update_ip(IPv4Address(six.u('127.0.0.1')))
